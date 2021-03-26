d07902dc99eb3b716a01fa2f02c4bb067fac9ed1
Tristan Rechenberger <rechenberger@me.com>
2021-03-26 16:42:48 +0100

    🔨 graphql extension

---
+++ graphql.config.js
@@ -0,0 +1,8 @@
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+module.exports = {
+  schema: 'https://rickandmortyapi.com/graphql',
+  // schema: './__generated__/graphql.schema.json',
+  // schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL,
+}
