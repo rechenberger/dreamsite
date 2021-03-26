1a5852bae4dec13e6da90236293e942ba4579cc2
Tristan Rechenberger <rechenberger@me.com>
2021-03-26 14:17:20 +0100

    ✨ useGetCharactersQuery

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { useGetCharactersQuery } from '../lib/graphql/operations/GetCharacters.graphql'
+
 export default function Home() {
+  const { data } = useGetCharactersQuery()
+  const characters = data?.characters
   return (
     <div>
       <h1>DreamSite</h1>
+      <pre>{JSON.stringify(characters, null, 2)}</pre>
     </div>
   )
 }
