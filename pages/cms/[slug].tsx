37f3b21972b8b579c59e980469cbbc8f4deafe71
Tristan Rechenberger <rechenberger@me.com>
2021-04-08 16:52:01 +0200

    ✨ CmsRoutePage queryGetPageBlocks

--- pages/cms/[slug].tsx
+++ pages/cms/[slug].tsx
@@ -2,6 +2,10 @@ import { MainLayout } from 'lib/components/MainLayout'
 import { Seo } from 'lib/components/Seo'
 import { SimpleGrid } from 'lib/components/SimpleGrid'
 import { SimpleJson } from 'lib/components/SimpleJson'
+import {
+  queryGetPageBlocks,
+  useGetPageBlocksQuery,
+} from 'lib/graphql/operations/GetPageBlocks.graphql'
 import { getStaticPathsPlus } from 'lib/next/getStaticPathsPlus'
 import { getStaticPropsPlus } from 'lib/next/getStaticPropsPlus'
 import { useParams } from 'lib/next/useParams'
@@ -9,27 +13,28 @@ import React from 'react'
 
 const CmsRoutePage = () => {
   const { slug } = useParams()
+  const { data } = useGetPageBlocksQuery({ variables: { slug } })
   return (
     <MainLayout>
       <Seo title={slug} />
       <SimpleGrid>
-        <SimpleJson value={{ slug }} />
+        <SimpleJson value={data} />
       </SimpleGrid>
     </MainLayout>
   )
 }
 
 export const getStaticProps = getStaticPropsPlus({
-  // getProps: async ({ params, apolloClient }) => {
-  //   const slug = params.slug as string
-  //   try {
-  //     await queryGetCharacter({ apolloClient, variables: { id } })
-  //   } catch (error) {
-  //     return {
-  //       notFound: true,
-  //     }
-  //   }
-  // },
+  getProps: async ({ params, apolloClient }) => {
+    const slug = params.slug as string
+    try {
+      await queryGetPageBlocks({ apolloClient, variables: { slug } })
+    } catch (error) {
+      return {
+        notFound: true,
+      }
+    }
+  },
 })
 
 export const getStaticPaths = getStaticPathsPlus({
