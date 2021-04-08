5a8d61bb0a52e60eef43cc7bd1cd7e17aaa834b3
Tristan Rechenberger <rechenberger@me.com>
2021-04-08 17:13:46 +0200

    ➕ react-tinacms-inline

--- lib/blocks/blocks/HeroBlock.tsx
+++ lib/blocks/blocks/HeroBlock.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { InlineForm, InlineText } from 'react-tinacms-inline'
+import styled from 'styled-components'
 import { useForm, usePlugin } from 'tinacms'
 import { BlockComponent, BlockType } from '../blocks'
 
 export interface HeroBlockConfig {
   mainText: string
 }
+
+const Heading = styled.h1`
+  font-size: 3em;
+  font-weight: 900;
+`
 
 const HeroBlock: BlockComponent<HeroBlockConfig> = ({ config, id, type }) => {
   const formConfig = {
@@ -29,7 +36,12 @@ const HeroBlock: BlockComponent<HeroBlockConfig> = ({ config, id, type }) => {
 
   return (
     <>
-      <h1>{betterConfig.mainText}</h1>
+      <InlineForm form={form}>
+        <Heading>
+          <InlineText name="mainText" />
+        </Heading>
+      </InlineForm>
+      <Heading>{betterConfig.mainText}</Heading>
     </>
   )
 }
