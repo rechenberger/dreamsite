a409e7b53bc513166014a00a4247a2c2e0c462ac
Tristan Rechenberger <rechenberger@me.com>
2021-04-08 17:43:35 +0200

    HeroBlock group

--- lib/blocks/blocks/HeroBlock.tsx
+++ lib/blocks/blocks/HeroBlock.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InlineForm, InlineGroup, InlineText } from 'react-tinacms-inline'
 import styled from 'styled-components'
+import tw from 'twin.macro'
 import { BlockComponent, BlockType } from '../blocks'
 import { useBlockTina } from '../useBlockTina'
 
@@ -11,6 +12,13 @@ export interface HeroBlockConfig {
 const Heading = styled.h1`
   font-size: 3em;
   font-weight: 900;
+  border: 1px solid black;
+`
+
+const HeroContainer = tw.div`
+  bg-gray-500
+  p-16
+  rounded
 `
 
 const HeroBlock: BlockComponent<HeroBlockConfig> = (block) => {
@@ -29,28 +37,32 @@ const HeroBlock: BlockComponent<HeroBlockConfig> = (block) => {
     <>
       <InlineForm form={form}>
         <InlineGroup
-          name="hero"
-          fields={[
-            {
-              name: 'typography.style',
-              label: 'Type Style',
-              description: 'Select a type style for the hero copy',
-              component: 'select',
-              options: ['Swiss-Style', 'Art-Nouveau', 'Command-Line'],
-            },
-            {
-              name: 'typography.color',
-              label: 'Type Color',
-              description: 'Select a color for the hero copy',
-              component: 'color',
-              widget: 'block',
-              colors: ['#404040', '#ff0000', '#1B1E25'],
-            },
-          ]}
+          name="."
+          fields={
+            [
+              {
+                name: 'typography.style',
+                label: 'Type Style',
+                description: 'Select a type style for the hero copy',
+                component: 'select',
+                options: ['Swiss-Style', 'Art-Nouveau', 'Command-Line'],
+              },
+              {
+                name: 'typography.color',
+                label: 'Type Color',
+                description: 'Select a color for the hero copy',
+                component: 'color',
+                widget: 'block',
+                colors: ['#404040', '#ff0000', '#1B1E25'],
+              },
+            ] as any // TODO: fix typing
+          }
         >
-          <Heading>
-            <InlineText name="mainText" />
-          </Heading>
+          <HeroContainer>
+            <Heading>
+              <InlineText name="mainText" />
+            </Heading>
+          </HeroContainer>
         </InlineGroup>
       </InlineForm>
       <Heading>{config.mainText}</Heading>
