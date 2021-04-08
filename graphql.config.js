ce46ea2d903b666dd4911d720624fc3c3f97ceac
Tristan Rechenberger <rechenberger@me.com>
2021-04-08 16:49:14 +0200

    🔧 extension use local schema

--- graphql.config.js
+++ graphql.config.js
@@ -2,7 +2,7 @@
 // dotenv.config()
 
 module.exports = {
-  schema: 'https://rickandmortyapi.com/graphql',
-  // schema: './__generated__/graphql.schema.json',
+  // schema: 'https://rickandmortyapi.com/graphql',
+  schema: './__generated__/graphql.schema.json',
   // schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL,
 }
