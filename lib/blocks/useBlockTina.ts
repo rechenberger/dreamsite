0640d9e8d7376aaa3ae6ea92d50f84306dc51078
Tristan Rechenberger <rechenberger@me.com>
2021-04-08 17:27:55 +0200

    ♻️ useBlockTina useMemo

--- lib/blocks/useBlockTina.ts
+++ lib/blocks/useBlockTina.ts
@@ -1,5 +1,6 @@
 import { FullBlockFragment } from 'lib/graphql/operations/GetPageBlocks.graphql'
 import { useSaveBlockMutation } from 'lib/graphql/operations/SaveBlock.graphql'
+import { useMemo } from 'react'
 import { useForm, usePlugin } from 'tinacms'
 
 export const useBlockTina = ({
@@ -12,17 +13,20 @@ export const useBlockTina = ({
   const [save] = useSaveBlockMutation()
   const { id, type, config: configBefore } = block
 
-  const formConfig = {
-    id,
-    label: type,
-    initialValues: configBefore,
-    onSubmit: async (config) => {
-      console.log({ config })
-      await save({ variables: { id, config } })
-      alert(`saved`)
-    },
-    fields,
-  }
+  const formConfig = useMemo(
+    () => ({
+      id,
+      label: type,
+      initialValues: configBefore,
+      onSubmit: async (config) => {
+        console.log({ config })
+        await save({ variables: { id, config } })
+        alert(`saved`)
+      },
+      fields,
+    }),
+    [id, configBefore, save]
+  )
 
   const [configAfter, form] = useForm(formConfig)
   usePlugin(form)
