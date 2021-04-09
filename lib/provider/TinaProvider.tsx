68084e180cf2cbf9ba7b0f97a33e41585b6d6383
Tristan Rechenberger <rechenberger@me.com>
2021-04-09 11:17:06 +0200

    ✨ TinaProvider toolbar

--- lib/provider/TinaProvider.tsx
+++ lib/provider/TinaProvider.tsx
@@ -2,6 +2,6 @@ import { FunctionComponent, useMemo } from 'react'
 import { TinaCMS, TinaProvider as TinaProviderRaw } from 'tinacms'
 
 export const TinaProvider: FunctionComponent<{}> = ({ children }) => {
-  const cms = useMemo(() => new TinaCMS({ enabled: true, sidebar: true }), [])
+  const cms = useMemo(() => new TinaCMS({ enabled: true, toolbar: true }), [])
   return <TinaProviderRaw cms={cms}>{children}</TinaProviderRaw>
 }
